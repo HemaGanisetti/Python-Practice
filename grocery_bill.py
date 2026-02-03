75a9c4b233922342d9c82adbaa36521f029de37a
Hema-AI <50709105+HemaGanisetti@users.noreply.github.com>
2026-02-03 11:27:09 -0700

    Update grocery_bill.py

--- grocery_bill.py
+++ grocery_bill.py
@@ -3,5 +3,7 @@ wheat = 10.05
 onion = 4.29
 eggs = 3.99
 total=(rice + wheat + onion + eggs)
-total_round=(round(total,2))
-print(total_round)
+total_round_1=(round(total,1))
+total_round_2=((rice + wheat + onion + eggs),2)
+print(total_round_1)
+peintt(total_round_2)
