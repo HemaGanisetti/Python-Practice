3ef92ec0f629ff799fdae59fca964fd184a25bb4
Hema-AI <50709105+HemaGanisetti@users.noreply.github.com>
2026-02-04 10:16:13 -0700

    Update and rename grocery_bill.py to grocery_bill_calculator.py

--- grocery_bill_calculator.py
+++ grocery_bill_calculator.py
@@ -3,7 +3,6 @@ wheat = 10.05
 onion = 4.29
 eggs = 3.99
 total=(rice + wheat + onion + eggs)
-total_round_1=(round(total,1))
+print(round(total,1))
 total_round_2=(round((rice + wheat + onion + eggs),2))
-print(total_round_1)
 print(total_round_2)
